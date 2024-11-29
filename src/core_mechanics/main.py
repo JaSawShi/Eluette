65b5621f35388402f76cd16b5ea5f3678f73edbe
Apolon-as <129028339+Apolon-as@users.noreply.github.com>
2024-11-30 00:02:20 +0500

    del withoutknowledge

--- src/core_mechanics/main.py
+++ src/core_mechanics/main.py
@@ -1,11 +1,12 @@
 from settings import *
-from sprites import Player
+from sprites import *
 
 pygame.init()
 screen = pygame.display.set_mode((s_w,s_h))
 clock = pygame.time.Clock()
 
-player = Player(100,100,screen)
+player = Player(500, 500, 56, 112, (255,255,255), screen, 8)
+ground = Real_Object(0, 704, 1024, 64, (255,255,255), screen)
 
 run = True
 while run:
@@ -16,6 +17,7 @@ while run:
             run = False
     
     screen.fill((0,0,0))
+    ground.draw()
 
     player.draw()
     player.move()
--- src/core_mechanics/sprites.py
+++ src/core_mechanics/sprites.py
@@ -1,14 +1,22 @@
 from settings import *
 
-class Player():
-    def __init__(self, pos_x, pos_y, display_surf):
-        self.image = pygame.Surface((56,112))
-        self.image.fill((255,255,255))
+class Real_Object():
+    def __init__(self, pos_x, pos_y, w, h, color, display_surf):
+        self.image = pygame.Surface((w,h))
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.rect.topleft = (pos_x,pos_y)
         self.display_surf = display_surf
-        self.speed = 5
-    
+
+    def draw(self):
+        self.display_surf.blit(self.image, self.rect)
+
+class Player(Real_Object):
+    def __init__(self, pos_x, pos_y, w, h, color, display_surf, speed):
+        super().__init__(pos_x, pos_y, w, h, color, display_surf)
+        self.speed = speed
+        self.on_ground
+
     def draw(self):
         self.display_surf.blit(self.image, self.rect)
 
@@ -19,3 +27,5 @@ class Player():
             self.rect.x += self.speed
         if keys[pygame.K_a]:
             self.rect.x -= self.speed
+        if keys[pygame.K_SPACE]:
+            self.rect.y -= 2
